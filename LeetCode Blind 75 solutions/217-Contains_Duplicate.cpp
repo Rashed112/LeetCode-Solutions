833205912156862525d49017926608c12c2c0b7c
Md. Rasheduzzaman <44283694+Rashed112@users.noreply.github.com>
2022-10-08 00:00:48 +0600

    unordered_hashmap

--- LeetCode Blind 75 solutions/217-Contains_Duplicate.cpp
+++ LeetCode Blind 75 solutions/217-Contains_Duplicate.cpp
@@ -11,3 +11,9 @@ public:
         return false;
     }
 };
+//TC: O(nlogn) //because of sorting
+//SC: O(1)
+
+/*but sacrificing the spcae complexity , we can improve time complexity..
+for this, we've to use an empty unordered_hashmap  and we'll check if a value is in the hashmap and if not then put it in the hashmap.
+*/
