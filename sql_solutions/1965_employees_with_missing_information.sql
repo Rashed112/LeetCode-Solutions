f15efa8c904cc195f600d0cee8e0f9cf13e93436
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-03-28 04:28:49 +0600

    Create 1965_employees_with_missing_information.sql

---
+++ sql_solutions/1965_employees_with_missing_information.sql
@@ -0,0 +1,7 @@
+select employee_id from employees
+where employee_id not in (select employee_id from salaries)
+union
+select employee_id from salaries
+where employee_id not in (select employee_id from employees)
+order by employee_id asc;
+
