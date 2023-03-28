04ee90102cee57344b246d4c4423ab8b3b4ee564
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-03-28 16:21:35 +0600

    Update 608_tree_node.sql

--- sql_solutions/608_tree_node.sql
+++ sql_solutions/608_tree_node.sql
@@ -7,3 +7,13 @@ union
 select id, 'Leaf' as type from tree
 where p_id is not null and id not in (select distinct p_id from tree where p_id is not null)
 order by id; 
+
+
+#Alternate solution:
+select id, 
+    case
+        when p_id is null then 'Root'
+        when id in (select p_id from tree where p_id is not null) then 'Inner'
+        else 'Leaf'
+        end as type
+from tree order by id;
