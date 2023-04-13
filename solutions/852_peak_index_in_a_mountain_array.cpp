e486a0cb5e385478c72d1d563dbbb16350fbde54
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-04-13 17:54:54 +0600

    Create 852_peak_index_in_a_mountain_array.cpp

---
+++ solutions/852_peak_index_in_a_mountain_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l=0, r=arr.size()-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(arr[m]<arr[m+1]){
+                l=m+1;
+            }
+            else{
+                r=m;
+            }
+        }
+        return l;
+    }
+};
