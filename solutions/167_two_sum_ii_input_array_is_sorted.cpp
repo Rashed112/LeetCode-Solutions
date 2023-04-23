8bab930c415d3d5071b21b4c3ae7c1090b121e45
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-04-24 01:30:35 +0600

    Create 167_two_sum_ii_input_array_is_sorted.cpp

---
+++ solutions/167_two_sum_ii_input_array_is_sorted.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size();
+        int l=0, r=n-1,m;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target){
+                return {l+1, r+1};
+            }
+            else if(numbers[l]+numbers[r] < target ){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return {l+1, r+1};
+    }
+};
