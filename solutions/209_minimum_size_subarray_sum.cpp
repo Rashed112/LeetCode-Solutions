36f019a3bbee399e9c5b8a3a25982bb719c7c2ff
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-05-22 02:09:51 +0600

    Update 209_minimum_size_subarray_sum.cpp

--- solutions/209_minimum_size_subarray_sum.cpp
+++ solutions/209_minimum_size_subarray_sum.cpp
@@ -1,4 +1,4 @@
-//two pointers
+//Solution-1: two pointers - O(n)
 
 class Solution {
 public:
@@ -13,5 +13,28 @@ public:
             }
         }
         return res == INT_MAX? 0 : res;
+    }
+};
+
+//Solution-2: Binary Search - O(nlog n)
+class Solution {
+    #define ll long long 
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        int ans = INT_MAX;
+        vector<int> sums(n+1, 0);
+        for(int i=1;i<=n;i++){
+            sums[i]= sums[i-1]+nums[i-1];
+        }
+        for(int i=0; i<n;i++){
+            int find = target+sums[i];
+            int idx = lower_bound(sums.begin(), sums.end(), find)- sums.begin();
+            if(idx != n+1){
+                ans = min(ans, idx-i);
+            }
+        }
+        return ans == INT_MAX? 0 : ans;
     }
 };
