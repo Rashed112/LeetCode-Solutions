ce1c6b7ae1f9943cecb28a7d53a87ea4224a3ac9
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-06-28 23:46:39 +0600

    stack
    monotonic stack

---
+++ solutions/84_largest_rectangle_in_histogram.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int res = 0;
+        stack<pair<int, int>>sp;
+        for(int i=0;i<heights.size();i++){
+            int start = i;
+            while(!sp.empty() && sp.top().second > heights[i]){
+                int index = sp.top().first;
+                int w = i - index;
+                int h = sp.top().second;
+                res = max(res, w*h);
+                sp.pop();
+                start = index;
+            }
+            sp.push({start, heights[i]});
+        }
+
+        while(!sp.empty()){
+            int w = heights.size() - sp.top().first;
+            int h = sp.top().second;
+            res = max(res, w*h);
+            sp.pop();
+        }
+        return res;
+    }
+};
+
+// time O(n)
+// space O(n)
