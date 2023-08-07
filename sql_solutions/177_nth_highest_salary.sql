f7e9beaa728f521dade166120b3315a6ca123e00
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-08-07 16:01:12 +0600

    VVI

--- sql_solutions/177_nth_highest_salary.sql
+++ sql_solutions/177_nth_highest_salary.sql
@@ -2,9 +2,8 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 SET N=N-1;
   RETURN (
-      # Write your MySQL query statement below.
       Select distinct(salary) from Employee
       Order by salary desc
-      LIMIT 1 OFFSET N  #Offset skips n rows and we can't do any calculation like n-1 here, so we had to set that out before
+      LIMIT 1 OFFSET N     #Offset skips n rows and we can't do any calculation like n-1 here, so we had to set that out before
   );
 END
