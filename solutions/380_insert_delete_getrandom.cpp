04443d9da43cf4a0d1bb4bc730a891965151c239
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-10-10 21:22:07 +0600

    desing

---
+++ solutions/380_insert_delete_getrandom.cpp
@@ -0,0 +1,48 @@
+class RandomizedSet {
+public:
+    unordered_map<int,int>indices;
+    vector<int>values;
+    RandomizedSet() {
+
+    }
+    
+    bool insert(int val) {
+        if(indices.find(val)==indices.end()){
+            values.push_back(val);
+            indices[val]=values.size()-1;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool remove(int val) {
+        if(indices.find(val)==indices.end()){
+            return false;
+        }
+        //find the index of last value
+        int tempIdx = indices[val];
+        //get the last value of vector
+        //and change it's idx to current value's idx
+        indices[values[values.size()-1]] = tempIdx;
+        //replace current values with last value
+        values[tempIdx] = values[values.size()-1];
+        indices.erase(val);
+        values.pop_back();
+        return true;
+        
+    }
+    
+    int getRandom() {
+        return values[rand()%values.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
