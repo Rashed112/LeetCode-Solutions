160bfebc4554902efe02651bef638cfcd69c66a0
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-12-02 01:13:36 +0600

    Create 1662_check_if_two_string_arrays_are_equivalent.cpp

---
+++ solutions/1662_check_if_two_string_arrays_are_equivalent.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string str1="", str2="";
+        for(int i=0;i<word1.size();i++){
+            str1+=word1[i];
+        }
+        for(int i=0;i<word2.size();i++){
+            str2+=word2[i];
+        }
+        if(str1==str2){
+            return true;
+        }
+        return false;
+    }
+};
+
+// Time: O(n)
+// Space: O(n)
