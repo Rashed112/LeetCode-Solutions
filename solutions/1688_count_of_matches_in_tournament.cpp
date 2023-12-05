99b245919dafd36c965cb969168f6e9744c2395c
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-12-05 12:08:28 +0600

    Create 1688_count_of_matches_in_tournament.cpp

---
+++ solutions/1688_count_of_matches_in_tournament.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int res=0;
+        while(n>1){
+            res+=n/2;
+            n-=n/2;
+        }
+        return res;
+    }
+};
+
+// Time: O(log n)
+// Space: O(1)
