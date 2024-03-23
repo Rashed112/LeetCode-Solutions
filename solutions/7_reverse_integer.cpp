7907dcedd41a92d9d32643996fb470ba0525295b
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2024-03-24 00:57:53 +0600

    math

---
+++ solutions/7_reverse_integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while(x){
+            if(res<INT_MIN/10 || res>INT_MAX/10){
+                return 0;
+            }
+            res = res*10 + x%10;
+            x/=10;
+        }
+        
+        return res;
+    }
+};
+
+// Time: O(log x)
+// Space: O(1)
