9f26ff1ecc3701ad6536748d52d3ab317cde0f95
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2024-05-25 00:45:34 +0600

    dp

--- solutions/416-Partition_equal_subset_sum.cpp
+++ solutions/416-Partition_equal_subset_sum.cpp
@@ -1,3 +1,4 @@
+// Approach - 1: 
 /*
 * we have to understand the subproblem, which is, "is it possible to make a certain sum from an array?"
 * then it's a 0/1 knapsack problem, which is whether we can take a certain index or not for making the sum
@@ -27,5 +28,39 @@ public:
     }  
     return func(nums.size()-1, sum/2, nums); 
 
+    }
+};
+
+
+// Approach - 2 :
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(auto x : nums){
+            sum += x;
+        }
+        if(sum%2){
+            return false;
+        }
+        int target = sum/2, n = nums.size();
+        int dp[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=target; j++){
+                if(j==0){
+                    dp[i][j]=1;
+                }
+                else if(i==0){
+                    dp[i][j]=0;
+                }
+                else if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][target];
     }
 };
